d13d6c9b23f3d0650a230ee1bd275d6d012e06a3
Creativefree.io <168169348+Creativefree@users.noreply.github.com>
2024-06-09 21:00:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        fontFamily: {
+          inter: ["Inter", "sans-serif"],
+          caveat: ["Caveat", "cursive"],
+        },
+      },
+    },
+  };
